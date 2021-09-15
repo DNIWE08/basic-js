b566cc019dbb81616807f7666cd0a1b5b3ca9aad
DNIWE08 <olimp12345678@gmail.com>
2021-09-15 22:01:41 +0300

    feat: add solutions

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ export default function deleteDigit(num) {
+  let result = 0,
+    num_digits = [];
+  while (num) {
+    num_digits.push(num % 10);
+    num = Math.floor(num / 10);
+  }
+  for (let index_num = 0; index_num < num_digits.length; index_num++) {
+    let n = 0;
+    for (let i = num_digits.length - 1; i >= 0; i--) {
+      if (i !== index_num) {
+        n = n * 10 + num_digits[i];
+      }
+    }
+    result = Math.max(n, result);
+  }
+  return result;
+} 
--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,21 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  const b = arr.slice();
+  let pos = [];
+  let i = -1;
+  while ((i = arr.indexOf(-1, i + 1)) > -1) {
+    pos.push(i);
+  }
+  const rpos = pos.slice();
+  while (rpos.length) {
+    b.splice(rpos.pop(), 1);
+  }
+
+  b.sort((arr, b) => arr - b);
+  while (pos.length) {
+    b.splice(pos.shift(), 0, -1);
+  }
+  return b;
 }
